07b530d4a40052aa3645e910fc9bbe763c9fcad8
jansen <jansen@bread>
2024-07-21 23:11:33 -0400

    made it so that the movez determines the base height

--- PuzzleFrame.groovy
+++ PuzzleFrame.groovy
@@ -1,5 +1,5 @@
 CSG puzzle = new Cube(200,200,2.5).toCSG().toZMax()
-CSG bottom = new Cube(210,210,1).toCSG().toZMax()
+CSG bottom = new Cube(210,210,1).toCSG().toZMin()
 			.movez(-5)
 
 CSG base = bottom.union(puzzle).hull().difference(puzzle)
