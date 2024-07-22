aed8bcc50ed4709f8f7bce9e669fb0cf9683cdbc
jansen <jansen@bread>
2024-07-21 23:02:36 -0400

    normalized the heights

--- PuzzleFrame.groovy
+++ PuzzleFrame.groovy
@@ -1,8 +1,9 @@
-CSG puzzle = new Cube(200,200,2.5).toCSG()
-CSG bottom = new Cube(210,210,1).toCSG()
+CSG puzzle = new Cube(200,200,2.5).toCSG().toZMax()
+CSG bottom = new Cube(210,210,1).toCSG().toZMax()
 			.movez(-5)
 
 CSG base = bottom.union(puzzle).hull().difference(puzzle)
+			.toZMin()
 
 return base
 //return [puzzle, bottom]
