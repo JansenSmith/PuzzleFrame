f8450665027bac3f35c3a3364b56f9786bca5e63
jansen <jansen@bread>
2024-07-22 13:14:58 -0400

    edges less steep angle

--- PuzzleFrame.groovy
+++ PuzzleFrame.groovy
@@ -1,5 +1,5 @@
 CSG puzzle = new Cube(195,195,2.6).toCSG().toZMax()
-CSG bottom = new Cube(210,210,1).toCSG().toZMin()
+CSG bottom = new Cube(208,208,1).toCSG().toZMin()
 			.movez(-5)
 
 CSG base = bottom.union(puzzle).hull().difference(puzzle)
